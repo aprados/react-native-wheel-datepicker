d56b8e62ee961122be75cb3a935374c6fecdc7aa
Ariel Prados <arielprados@gmail.com>
2018-12-12 16:09:26 -0300

    Update picker.js

--- src/picker.js
+++ src/picker.js
@@ -60,6 +60,8 @@ export default class Picker extends Component {
       >
         {pickerData.map((data, index) => (
           <PickerItem
+            accessible={true}
+            accessibilityLabel={typeof data.label !== 'undefined' ? data.label : data.toString()}
             key={index}
             value={typeof data.value !== 'undefined' ? data.value : data}
             label={typeof data.label !== 'undefined' ? data.label : data.toString()}
